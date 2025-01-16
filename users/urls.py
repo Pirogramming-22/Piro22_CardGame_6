bb4b1f7ed7cedb922780b68f0b54725ec50983d2
blueoxygens <exboxer234@gmail.com>
2025-01-16 21:55:18 +0900

    naver login 구현 완료

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('login/', auth_view.LoginView.as_view(), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
