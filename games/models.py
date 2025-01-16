0614ffb26c21427f8917153fee8268e6518c0b22
blueoxygens <exboxer234@gmail.com>
2025-01-16 17:13:30 +0900

    model 구현 에러 해결

--- games/models.py
+++ games/models.py
@@ -2,10 +2,36 @@ from django.db import models
 from users.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
 class Game(models.Model):
-    attacker_id = models.ForeignKey(User, verbose_name="공격자 아이디", on_delete=models.CASCADE)
-    defender_id = models.ForeignKey(User, verbose_name="방어자 아이디", on_delete=models.CASCADE)
-    game_result = models.IntegerField('게임 결과',default=0)
-    attacker_chosen_card = models.IntegerField('공격자 카드',[MinValueValidator(1), MaxValueValidator(9)])
-    defender_chosen_card = models.IntegerField('방어자 카드',[MinValueValidator(1), MaxValueValidator(9)])
+    attacker = models.ForeignKey(
+        User, 
+        verbose_name="공격자", 
+        on_delete=models.CASCADE, 
+        related_name="attacker_games"
+    )
+    defender = models.ForeignKey(
+        User, 
+        verbose_name="방어자", 
+        on_delete=models.CASCADE, 
+        related_name="defender_games"
+    )
+    game_result = models.IntegerField(
+        verbose_name="게임 결과", 
+        default=0
+    )
+    attacker_chosen_card = models.IntegerField(
+        verbose_name="공격자가 선택한 카드", 
+        validators=[MinValueValidator(1), MaxValueValidator(9)]
+    )
+    defender_chosen_card = models.IntegerField(
+        verbose_name="방어자가 선택한 카드", 
+        validators=[MinValueValidator(1), MaxValueValidator(9)]
+    )
+
+    class Meta:
+        verbose_name = "게임"
+        verbose_name_plural = "게임들"
+        ordering = ["-id"]
+
+    def __str__(self):
+        return f"Game {self.id}: {self.attacker} vs {self.defender}"
