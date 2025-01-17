d9f28b4db879a323f2a722e9064c3decced0b90e
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-01-18 03:43:53 +0900

    [add]: 마지막 업데이트

--- games/views.py
+++ games/views.py
@@ -4,4 +4,4 @@ def game_main(request):
     context = {
         'data': 'hello!',
     }
-    return render(request, 'games/game_attack.html', context)
+    return render(request, 'games/game_defense.html', context)
