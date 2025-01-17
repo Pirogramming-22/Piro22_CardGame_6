5399e11ae107484c6d9067c40ef60dc4c8866b64
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-01-17 17:48:21 +0900

    merge

--- games/views.py
+++ games/views.py
@@ -4,4 +4,4 @@ def game_main(request):
     context = {
         'data': 'hello!',
     }
-    return render(request, 'games/counter_attack.html', context)
+    return render(request, 'games/delete_attack.html', context)
