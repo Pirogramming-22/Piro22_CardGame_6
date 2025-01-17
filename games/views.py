f824f3efed9dba494ffe642e097b8e3e3e81ced6
iseun15 <ave0315@naver.com>
2025-01-17 18:42:03 +0900

    [feat] games_.html css 수정

--- games/views.py
+++ games/views.py
@@ -4,4 +4,4 @@ def game_main(request):
     context = {
         'data': 'hello!',
     }
-    return render(request, 'games/ranking.html', context)
+    return render(request, 'games/game_attack.html', context)
