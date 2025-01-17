755fd91d6c9af901ad17a8625122aa3a85742c70
iseun15 <ave0315@naver.com>
2025-01-17 17:05:01 +0900

    game html 수정

--- games/views.py
+++ games/views.py
@@ -4,4 +4,4 @@ def game_main(request):
     context = {
         'data': 'hello!',
     }
-    return render(request, 'games/finished_attack.html', context)
+    return render(request, 'games/game_attack.html', context)
