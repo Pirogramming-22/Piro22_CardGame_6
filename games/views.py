71948d70a8f907db86642c0bb2521fd52a891014
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-01-17 17:52:24 +0900

    [fix]: merge conflict 수정

--- games/views.py
+++ games/views.py
@@ -4,8 +4,4 @@ def game_main(request):
     context = {
         'data': 'hello!',
     }
-<<<<<<< HEAD
-    return render(request, 'games/delete_attack.html', context)
-=======
     return render(request, 'games/game_defense.html', context)
->>>>>>> 00d6a8eafe8fd5eecb23b9a7f7d254846b3f8f43
